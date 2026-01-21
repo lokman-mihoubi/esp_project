b0a72ccb93c151e4b7fb8f2259c20fe653a96bf8
StartAlg <112264370+lokman-mihoubi@users.noreply.github.com>
2026-01-21 15:58:34 +0100

    Update auth.ts

--- anfu_frontend/lib/auth.ts
+++ anfu_frontend/lib/auth.ts
@@ -4,9 +4,10 @@ const API = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/auth/`,
 });
 
-export const register = async (username: string, password: string) => {
-  return API.post('register/', { username, password });
+export const register = async (username: string, password: string, region: string) => {
+  return API.post('register/', { username, password, region });
 };
+
 
 export const login = async (username: string, password: string) => {
   return API.post('login/', { username, password });
