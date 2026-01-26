2dcbd7218f5bc023151791277331958c870c2bef
StartAlg <112264370+lokman-mihoubi@users.noreply.github.com>
2026-01-26 08:10:24 +0100

    Update page.tsx

--- anfu_frontend/app/dashboard/page.tsx
+++ anfu_frontend/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
-"use client";
+'use client';
 import React from "react";
-import { useState } from "react";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { logout } from '@/lib/auth';
 import { User, Foncier, Document, Task, StepType,Usage  } from '@/types';
