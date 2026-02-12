48906ff41f002acce409da7d436a3dd7ccf8ef33
StartAlg <112264370+lokman-mihoubi@users.noreply.github.com>
2026-02-12 10:01:06 +0100

    Create wilayaToRegion.ts

---
+++ anfu_frontend/types/wilayaToRegion.ts
@@ -0,0 +1,31 @@
+// wilayaToRegion.ts
+export const WILAYA_TO_REGION: Record<string, string> = {
+  // CENTRE
+  "03": "DRC", "44": "DRC", "42": "DRC", "26": "DRC",
+  "16": "DRC", "09": "DRC", "15": "DRC", "10": "DRC",
+  "28": "DRC", "17": "DRC", "06": "DRC", "34": "DRC",
+  "35": "DRC",
+
+  // EST
+  "19": "DRE", "18": "DRE", "05": "DRE", "40": "DRE",
+  "12": "DRE", "04": "DRE", "41": "DRE", "24": "DRE",
+  "36": "DRE", "23": "DRE", "21": "DRE", "43": "DRE",
+  "25": "DRE",
+
+  // OUEST
+  "22": "DRO", "13": "DRO", "46": "DRO", "31": "DRO",
+  "20": "DRO", "29": "DRO", "14": "DRO", "48": "DRO",
+  "27": "DRO", "38": "DRO", "02": "DRO",
+
+  // SUD
+  "50": "DRGS", "54": "DRGS", "11": "DRGS", "56": "DRGS",
+
+  // SUD EST
+  "33": "DRSE", "30": "DRSE", "53": "DRSE", "58": "DRSE",
+  "47": "DRSE", "51": "DRSE", "07": "DRSE", "57": "DRSE",
+  "55": "DRSE", "39": "DRSE",
+
+  // SUD OUEST
+  "01": "DRSO", "49": "DRSO", "52": "DRSO", "37": "DRSO",
+  "08": "DRSO", "45": "DRSO", "32": "DRSO",
+};
